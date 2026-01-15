dc741aa8dc53d01bd01290f08725b69574a94ea9
JINO-ROHIT <find.jinorohit@gmail.com>
2026-01-15 21:40:47 +0530

    feat: rope embed

--- qwen_0.6B.py
+++ qwen_0.6B.py
@@ -18,6 +18,30 @@ QWEN3_CONFIG = {
     "dtype": torch.bfloat16,     
 }
 
+def compute_rope_params(head_dim: int, theta_base: int = 10_000, context_length: int = 4096, dtype: torch.dtype = torch.float32):
+    inv_freq = 1.0 / theta_base ** (torch.arange(0, head_dim, 2, dtype = dtype) / head_dim)
+    positions = torch.arange(0, context_length, dtype = dtype)
+
+    angles = positions.unsqueeze(1) * inv_freq.unsqueeze(0) # [ctx_len, 1] * [1, head_dim / 2]
+    angles = torch.cat([angles, angles], dim = 1) # match to head dim
+
+    sin_angle = torch.sin(angles)
+    cos_angle = torch.cos(angles)
+    return sin_angle, cos_angle
+
+def apply_rope(x: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor):
+    bs, n_head, seq_len, head_dim = x.shape
+    x1 = x[..., : head_dim // 2] # ... better than use :, :, :, :head_dim //2
+    x2 = x[..., head_dim // 2 :]
+
+    cos = cos[:seq_len, :].unsqueeze(0).unsqueeze(0) # (1, 1, seq_len, head_dim)
+    sin = sin[:seq_len, :].unsqueeze(0).unsqueeze(0)
+
+    rotated = torch.cat((-x2, x1), dim = -1)
+    x_rotated = (x * cos) + (rotated * sin)
+    return x_rotated.to(dtype = x.dtype) # okay to use lower precision
+
+
 class RMSNorm(nn.Module):
     def __init__(self, config: dict, eps: float = 1e-5, bias: bool = False):
         super().__init__()
