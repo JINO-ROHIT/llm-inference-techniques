5559cc56b7169f4c036d50c9353691b491f12d03
JINO-ROHIT <find.jinorohit@gmail.com>
2026-01-18 22:59:36 +0530

    complete arch + tokenize

--- qwen_0.6B.py
+++ qwen_0.6B.py
@@ -1,8 +1,17 @@
 """a lot of the code is inspired from sebastian rascka here - https://github.com/rasbt/LLMs-from-scratch/blob/main/ch05/11_qwen3/standalone-qwen3.ipynb"""
 
+from pathlib import Path
+
 import torch
 import torch.nn as nn
+from safetensors.torch import load_file
+from huggingface_hub import hf_hub_download
 
+from utils import load_weights_into_qwen, Qwen3Tokenizer
+
+torch.manual_seed(42)
+
+device = torch.device("mps") # i have a mac
 
 QWEN3_CONFIG = {
     "vocab_size": 151_936,    
@@ -43,11 +52,11 @@ def apply_rope(x: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor):
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, config: dict, eps: float = 1e-5, bias: bool = False):
+    def __init__(self, emb_dim: int, eps: float = 1e-5, bias: bool = False):
         super().__init__()
         self.eps = eps
-        self.scale = nn.Parameter(torch.ones(config["emb_dim"]))
-        self.shift = nn.Parameter(torch.zeros(config["emb_dim"])) if bias else None
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim)) if bias else None
 
     def forward(self, x: torch.Tensor): # to-do write this better?
         input_dtype = x.dtype
@@ -62,7 +71,7 @@ class RMSNorm(nn.Module):
         
         return norm_x.to(input_dtype)
 
-class FNN:
+class FFN(nn.Module):
     def __init__(self, config: dict):
         super().__init__()
         self.gate_proj = nn.Linear(config["emb_dim"], config["hidden_dim"], dtype=config["dtype"], bias=False)
@@ -74,3 +83,160 @@ class FNN:
         gate = nn.functional.silu(gate)
         up = self.up_proj(x)
         return self.down_proj(gate * up)
+
+class GroupedQueryAttention(nn.Module):
+    def __init__(
+        self, d_in, num_heads, num_kv_groups, head_dim=None, qk_norm=False, dtype=None
+    ):
+        super().__init__()
+        assert num_heads % num_kv_groups == 0, "num_heads must be divisible by num_kv_groups"
+
+        self.num_heads = num_heads
+        self.num_kv_groups = num_kv_groups
+        self.group_size = num_heads // num_kv_groups
+
+        if head_dim is None:
+            assert d_in % num_heads == 0, "`d_in` must be divisible by `num_heads` if `head_dim` is not set"
+            head_dim = d_in // num_heads
+
+        self.head_dim = head_dim
+        self.d_out = num_heads * head_dim
+
+        self.W_query = nn.Linear(d_in, self.d_out, bias=False, dtype=dtype)
+        self.W_key = nn.Linear(d_in, num_kv_groups * head_dim, bias=False, dtype=dtype)
+        self.W_value = nn.Linear(d_in, num_kv_groups * head_dim, bias=False, dtype=dtype)
+
+        self.out_proj = nn.Linear(self.d_out, d_in, bias=False, dtype=dtype)
+
+        if qk_norm:
+            self.q_norm = RMSNorm(head_dim, eps=1e-6)
+            self.k_norm = RMSNorm(head_dim, eps=1e-6)
+        else:
+            self.q_norm = self.k_norm = None
+
+    def forward(self, x, mask, cos, sin):
+        b, num_tokens, _ = x.shape
+
+        queries = self.W_query(x)  # (b, num_tokens, num_heads * head_dim)
+        keys = self.W_key(x)       # (b, num_tokens, num_kv_groups * head_dim)
+        values = self.W_value(x)   # (b, num_tokens, num_kv_groups * head_dim)
+
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim).transpose(1, 2)
+        keys = keys.view(b, num_tokens, self.num_kv_groups, self.head_dim).transpose(1, 2)
+        values = values.view(b, num_tokens, self.num_kv_groups, self.head_dim).transpose(1, 2)
+
+        if self.q_norm:
+            queries = self.q_norm(queries)
+        if self.k_norm:
+            keys = self.k_norm(keys)
+
+        queries = apply_rope(queries, cos, sin)
+        keys = apply_rope(keys, cos, sin)
+
+        # Expand K and V to match number of heads
+        keys = keys.repeat_interleave(self.group_size, dim=1)
+        values = values.repeat_interleave(self.group_size, dim=1)
+
+        # Attention
+        attn_scores = queries @ keys.transpose(2, 3)
+        attn_scores = attn_scores.masked_fill(mask, -torch.inf)
+        attn_weights = torch.softmax(attn_scores / self.head_dim**0.5, dim=-1)
+
+        context = (attn_weights @ values).transpose(1, 2).reshape(b, num_tokens, self.d_out)
+        return self.out_proj(context)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = GroupedQueryAttention(
+            d_in=cfg["emb_dim"],
+            num_heads=cfg["n_heads"],
+            head_dim=cfg["head_dim"],
+            num_kv_groups=cfg["n_kv_groups"],
+            qk_norm=cfg["qk_norm"],
+            dtype=cfg["dtype"]
+        )
+        self.ff = FFN(cfg)
+        self.norm1 = RMSNorm(cfg["emb_dim"], eps=1e-6)
+        self.norm2 = RMSNorm(cfg["emb_dim"], eps=1e-6)
+
+    def forward(self, x, mask, cos, sin):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x, mask, cos, sin)  # Shape [batch_size, num_tokens, emb_size]
+        x = x + shortcut 
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = x + shortcut 
+
+        return x
+
+
+class Qwen3Model(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"], dtype=cfg["dtype"])
+
+        self.trf_blocks = nn.ModuleList(  
+            [TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+
+        self.final_norm = RMSNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False, dtype=cfg["dtype"])
+
+        if cfg["head_dim"] is None:
+            head_dim = cfg["emb_dim"] // cfg["n_heads"]
+        else:
+            head_dim = cfg["head_dim"]
+        cos, sin = compute_rope_params(
+            head_dim=head_dim,
+            theta_base=cfg["rope_base"],
+            context_length=cfg["context_length"]
+        )
+        self.register_buffer("cos", cos, persistent=False)
+        self.register_buffer("sin", sin, persistent=False)
+        self.cfg = cfg
+
+
+    def forward(self, in_idx):
+        tok_embeds = self.tok_emb(in_idx)
+        x = tok_embeds
+
+        num_tokens = x.shape[1]
+        mask = torch.triu(torch.ones(num_tokens, num_tokens, device=x.device, dtype=torch.bool), diagonal=1)
+        
+        for block in self.trf_blocks:
+            x = block(x, mask, self.cos, self.sin)
+        x = self.final_norm(x)
+        logits = self.out_head(x.to(self.cfg["dtype"]))
+        return logits
+    
+model = Qwen3Model(QWEN3_CONFIG)
+
+repo_id = f"Qwen/Qwen3-0.6B-Base"
+local_dir = Path(repo_id).parts[-1]
+weights_file = hf_hub_download(
+        repo_id=repo_id,
+        filename="model.safetensors",
+        local_dir=local_dir,
+    )
+weights_dict = load_file(weights_file)
+
+
+tokenizer = Qwen3Tokenizer(
+        tokenizer_file_path="Qwen3-0.6B-Base/tokenizer.json",
+        repo_id=repo_id,
+        apply_chat_template=False,
+        add_generation_prompt=False,
+        add_thinking=False
+)
+
+prompt = "Give me a short introduction to large language models."
+
+input_token_ids = tokenizer.encode(prompt)
+text = tokenizer.decode(input_token_ids)
+print(f"Query: {text}")
